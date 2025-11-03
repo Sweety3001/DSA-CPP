8a0acf639d8f0852e02f161a39dad975b4163336
Sweety3001 <sweetylog19@gmail.com>
2025-11-03 17:20:55 +0000

    basic cpp

---
+++ str.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    getline(cin, str);
+    cout<<str;
+
+
+    /*
+    DATATYPES
+    int, long, long long, float, double
+    string and getline
+    char
+    */
+    return 0;
+}
